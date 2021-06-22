4b879f803c0e11670c46d13ee8e61fbff423092b
Nicolas Truksinas <ntruksinas@callistech.com>
2021-06-22 19:58:46 -0300

    Initial Structure

---
+++ src/main/java/com/restteam/ong/ONGApplication.java
@@ -0,0 +1,11 @@
+package com.restteam.ong;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ONGApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(ONGApplication.class, args);
+    }
+}
