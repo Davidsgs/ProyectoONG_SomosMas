4b28a70fc927e1be51ead6bb70982de93e392eb1
David Garcia <davidsamuelgarciasanchez@gmail.com>
2021-06-23 18:05:11 -0300

    Ahora el RoleRepository hereda de CrudRepository

--- src/main/java/com/restteam/ong/repositories/RoleRepository.java
+++ src/main/java/com/restteam/ong/repositories/RoleRepository.java
@@ -1,7 +1,7 @@
 package com.restteam.ong.repositories;
 
 import com.restteam.ong.models.Role;
-import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.repository.CrudRepository;
 
-public interface RoleRepository extends JpaRepository<Role,Long> {
+public interface RoleRepository extends CrudRepository<Role,Long> {
 }
