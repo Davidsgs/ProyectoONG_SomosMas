c8bb97bf88f8b97f524e52b2ae8726fa8ae01a53
David Garcia <davidsamuelgarciasanchez@gmail.com>
2021-06-23 12:59:29 -0300

    Se crearon los Roles y su controller base.

---
+++ src/main/java/com/restteam/ong/controllers/RoleController.java
@@ -0,0 +1,7 @@
+package com.restteam.ong.controllers;
+
+import org.springframework.stereotype.Controller;
+
+@Controller
+public class RoleController {
+}
---
+++ src/main/java/com/restteam/ong/models/Role.java
@@ -0,0 +1,34 @@
+package com.restteam.ong.models;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
+import org.springframework.lang.NonNull;
+import javax.persistence.*;
+import java.util.Date;
+
+@Data
+@Entity
+@NoArgsConstructor
+@AllArgsConstructor
+@Table(name = "roles")
+public class Role {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(unique = true, nullable = false)
+    private Long id;
+
+    @NonNull
+    private String name;
+
+    private String description;
+
+    @CreationTimestamp
+    private Date created_at;
+
+    @UpdateTimestamp
+    private Date updated_at;
+}
