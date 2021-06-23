df426290c30105eb8dc404bdfba50a73dce6fb2b
David Garcia <davidsamuelgarciasanchez@gmail.com>
2021-06-23 13:15:12 -0300

    Se agregó el service de User y las inyecciones correspondientes en las otras clases.

--- src/main/java/com/restteam/ong/repositories/UserRepository.java
+++ src/main/java/com/restteam/ong/repositories/UserRepository.java
@@ -1,7 +1,8 @@
 package com.restteam.ong.repositories;
+import com.restteam.ong.models.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface UserRepository extends JpaRepository {
+public interface UserRepository extends JpaRepository<User,Long> {
 }
