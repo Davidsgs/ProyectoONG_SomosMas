de6b0ce6bd2e5be38ec344aa25ceda6e7fa1f738
Hector Gonzalez <hectorgonzalezuno@gmail.com>
2021-06-24 13:34:27 -0300

    activities review

--- src/main/java/com/restteam/ong/models/User.java
+++ src/main/java/com/restteam/ong/models/User.java
@@ -1,15 +1,21 @@
 package com.restteam.ong.models;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.Table;
+
+import org.hibernate.annotations.SQLDelete;
+import org.hibernate.annotations.Where;
+import org.springframework.lang.NonNull;
+
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
-import org.hibernate.annotations.CreationTimestamp;
-import org.hibernate.annotations.SQLDelete;
-import org.hibernate.annotations.UpdateTimestamp;
-import org.hibernate.annotations.Where;
-import org.springframework.lang.NonNull;
-import javax.persistence.*;
-import java.util.Date;
 
 @Entity
 @Data
