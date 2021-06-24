3da1bbe9685373b2f6e713f334386b12a4b5086d
Hector Gonzalez <hectorgonzalezuno@gmail.com>
2021-06-23 22:09:15 -0300

    Crear modelo, controlador y repositorio base de Activities

--- src/main/java/com/restteam/ong/services/ActivityService.java
+++ src/main/java/com/restteam/ong/services/ActivityService.java
@@ -1,4 +1,95 @@
 package com.restteam.ong.services;
 
+import java.util.ArrayList;
+import java.util.Optional;
+
+//import com.restteam.ong.controllers.dto.ActivityDTO;
+import com.restteam.ong.models.Activity;
+import com.restteam.ong.repositories.ActivityRepository;
+
+//import org.modelmapper.ModelMapper;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
 public class ActivityService {
+    @Autowired
+    ActivityRepository activityRepository;
+  /*  ModelMapper modelMapper = new ModelMapper();
+
+    public ArrayList<ActivityDTO> getActivities() {
+        ArrayList<Activity> activities = (ArrayList<Activity>) this.activityRepository.findAll();
+        ArrayList<ActivityDTO> activitiesDTO = new ArrayList<>();
+
+        for (int i = 0; i < activities.size(); i++) {
+            ActivityDTO aux = new ActivityDTO();
+            modelMapper.map(activities.get(i), aux);
+            activitiesDTO.add(aux);
+        }
+        return activitiesDTO;
+    }*/
+
+    public ArrayList<Activity> getActivities(){
+        return (ArrayList<Activity>)this.activityRepository.findAll();
+    }
+/*
+    public Optional<ActivityDTO> getActivityById(Long id) {
+        Optional<Activity> activity = activityRepository.findById(id);
+
+        if (activity != null) {
+            ActivityDTO activityDTO = new ActivityDTO();
+            modelMapper.map(activity, activityDTO);
+            return Optional.of(activityDTO);
+        } else {
+            return null;
+        }
+    }*/
+
+    public Optional<Activity> getActivityById(Long id){
+      return this.activityRepository.findById(id); //SI NO EXISTE DEVUELVE NULL
+    }
+
+/*
+    public Optional<ActivityDTO> getActivityByName(String name) {
+        Optional<Activity> activity = activityRepository.findByName(name);
+
+        if (activity != null) {
+            ActivityDTO activityDTO = new ActivityDTO();
+            modelMapper.map(activity, activityDTO);
+            return Optional.of(activityDTO);
+        } else {
+            return null;
+        }
+    }
+*/
+
+    public Optional<Activity> getActivityByName(String name){
+        return this.activityRepository.findByName(name);//SI NO EXISTE DEVUELVE NULL
+    }
+
+
+
+    public Activity postActivity(Activity activity) {
+        return this.activityRepository.save(activity);
+    }
+
+    public Optional<Activity> updateActivity(Activity activity, Long id) {
+        Optional<Activity> myActivity = activityRepository.findById(id);
+
+        if (myActivity != null) {
+            Activity activitySaved = this.activityRepository.save(activity);
+            return Optional.of(activitySaved);
+        } else {
+            return null;
+        }
+    }
+
+    public String deleteActivity(Long id) {
+        try {
+            this.activityRepository.deleteById(id);
+            return "Activity " + id + " successfully deleted";
+        } catch (Exception e) {
+            return "failed to delete activity " + id + "\nERROR\n\n" + e;
+        }
+    }
 }
