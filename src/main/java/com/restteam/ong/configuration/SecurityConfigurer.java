6f6aa6081f82d355b7ec7f595255634ea2ecca7c
David Garcia <davidsamuelgarciasanchez@gmail.com>
2021-06-27 00:25:46 -0300

    Se implemento el uso de Spring Security y JWT.

---
+++ src/main/java/com/restteam/ong/configuration/SecurityConfigurer.java
@@ -0,0 +1,58 @@
+package com.restteam.ong.configuration;
+
+import com.restteam.ong.services.impl.UserDetailsServiceImpl;
+import com.restteam.ong.util.JwtRequestFilter;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfigurer extends WebSecurityConfigurerAdapter {
+
+    @Autowired
+    UserDetailsServiceImpl userDetailsService;
+
+    @Autowired
+    private JwtRequestFilter jwtRequestFilter;
+
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+        auth.userDetailsService(userDetailsService);
+    }
+
+    protected void configure(HttpSecurity httpSecurity) throws Exception {
+        httpSecurity.csrf().disable().authorizeRequests()
+                //Acá, RUTAS PUBLICAS. (Cualquier usuario puede acceder a ellas.)
+                .antMatchers("/auth/register","/auth/login").permitAll()
+                //Acá, RUTAS PRIVADAS. (Solo acceden usuarios registrados y admins.)
+                .antMatchers("/").hasAnyAuthority("ROLE_USER","ROLE_ADMIN")
+                //Acá, RUTAS SOLO DE ADMINS.
+                .antMatchers("/").hasAnyAuthority("ROLE_ADMIN")
+                .anyRequest().authenticated()
+                .and().sessionManagement()
+                .sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+                .and().exceptionHandling();
+        httpSecurity.addFilterBefore(jwtRequestFilter, UsernamePasswordAuthenticationFilter.class);
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder(){
+        return new BCryptPasswordEncoder();
+    }
+
+    @Override
+    @Bean
+    protected AuthenticationManager authenticationManager() throws Exception {
+        return super.authenticationManager();
+    }
+}
