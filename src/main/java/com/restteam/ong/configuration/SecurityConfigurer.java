aecf40baaccbac64cd6742b31e40c78220a74b48
David Garcia <davidsamuelgarciasanchez@gmail.com>
2021-06-28 22:42:26 -0300

    Se agregó la ruta para poder ver la info de usuario registrado.

--- src/main/java/com/restteam/ong/configuration/SecurityConfigurer.java
+++ src/main/java/com/restteam/ong/configuration/SecurityConfigurer.java
@@ -35,7 +35,7 @@ public class SecurityConfigurer extends WebSecurityConfigurerAdapter {
                 //Acá, RUTAS PUBLICAS. (Cualquier usuario puede acceder a ellas.)
                 .antMatchers("/auth/register","/auth/login").permitAll()
                 //Acá, RUTAS PRIVADAS. (Solo acceden usuarios registrados y admins.)
-                .antMatchers("/users/**").hasAnyAuthority("ROLE_USER","ROLE_ADMIN")
+                .antMatchers("/users/**","/auth/me").hasAnyAuthority("ROLE_USER","ROLE_ADMIN")
                 //Acá, RUTAS SOLO DE ADMINS.
                 .antMatchers("/users").hasAnyAuthority("ROLE_ADMIN")
                 .anyRequest().authenticated()
