6e5b84a3c34105008255d9baa60e64e658503562
leonelmperalta <leonelm.peralta@outlook.es>
2021-06-30 16:02:32 -0300

    model de contacts ok

---
+++ src/main/java/com/restteam/ong/models/Contacts.java
@@ -0,0 +1,29 @@
+package com.restteam.ong.models;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import lombok.*;
+import org.hibernate.annotations.*;
+
+import javax.persistence.*;
+import javax.persistence.Entity;
+import javax.persistence.Table;
+
+@Entity
+@Table(name="contacts")
+@SQLDelete(sql = "UPDATE contacts SET deleted = true WHERE id=?")
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+public class Contacts {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(unique = true,nullable = false)
+    private Long id;
+    private Integer phone;
+    private String email;
+    private String message;
+    @Column(columnDefinition = "boolean default false")
+    private Boolean deleted = Boolean.FALSE;
+    private Long deletedAt;
+}
