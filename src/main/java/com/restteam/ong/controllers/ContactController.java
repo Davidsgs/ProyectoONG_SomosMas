0e381d6123b15b81242a25b3ed8057b693968242
leonelmperalta <leonelm.peralta@outlook.es>
2021-07-01 14:45:02 -0300

    endpoint para listar contactos ok

--- src/main/java/com/restteam/ong/controllers/ContactController.java
+++ src/main/java/com/restteam/ong/controllers/ContactController.java
@@ -5,10 +5,9 @@ import com.restteam.ong.services.ContactService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/contacts")
@@ -28,5 +27,16 @@ public class ContactController {
             return new ResponseEntity<>("We are sorry. Something failed, try again later.", HttpStatus.INTERNAL_SERVER_ERROR);
         }
         return new ResponseEntity<>("Contact created successfully ", HttpStatus.OK);
+    }
+
+    @GetMapping
+    public ResponseEntity<?> getContacts(){
+        List<Contact> contacts;
+        try {
+            contacts = contactService.getContacts();
+        } catch (Exception e) {
+            return new ResponseEntity<>("We are sorry, something failed. Try again later.", HttpStatus.INTERNAL_SERVER_ERROR);
+        }
+        return new ResponseEntity<>(contacts,HttpStatus.OK);
     }
 }
--- src/main/java/com/restteam/ong/services/ContactService.java
+++ src/main/java/com/restteam/ong/services/ContactService.java
@@ -3,7 +3,11 @@ package com.restteam.ong.services;
 import com.restteam.ong.models.Contact;
 import com.restteam.ong.repositories.ContactRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @Service
 public class ContactService {
@@ -13,5 +17,9 @@ public class ContactService {
 
     public void createContact(Contact contact) {
         contactRepository.save(contact);
+    }
+
+    public List<Contact> getContacts() {
+        return contactRepository.findAll();
     }
 }
