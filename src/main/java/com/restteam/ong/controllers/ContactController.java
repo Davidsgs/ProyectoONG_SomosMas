d77b112074a204f59c3060613683d3adc941eb99
leonelmperalta <leonelm.peralta@outlook.es>
2021-07-01 15:15:08 -0300

    agregado contactDTO

--- src/main/java/com/restteam/ong/controllers/ContactController.java
+++ src/main/java/com/restteam/ong/controllers/ContactController.java
@@ -1,5 +1,6 @@
 package com.restteam.ong.controllers;
 
+import com.restteam.ong.controllers.dto.ContactDTO;
 import com.restteam.ong.models.Contact;
 import com.restteam.ong.services.ContactService;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -7,6 +8,7 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.ArrayList;
 import java.util.List;
 
 @RestController
@@ -31,7 +33,7 @@ public class ContactController {
 
     @GetMapping
     public ResponseEntity<?> getContacts(){
-        List<Contact> contacts;
+        List<ContactDTO> contacts;
         try {
             contacts = contactService.getContacts();
         } catch (Exception e) {
--- src/main/java/com/restteam/ong/services/ContactService.java
+++ src/main/java/com/restteam/ong/services/ContactService.java
@@ -1,10 +1,12 @@
 package com.restteam.ong.services;
 
+import com.restteam.ong.controllers.dto.ContactDTO;
 import com.restteam.ong.models.Contact;
 import com.restteam.ong.repositories.ContactRepository;
+import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
+import org.springframework.ui.ModelMap;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -14,12 +16,23 @@ public class ContactService {
 
     @Autowired
     ContactRepository contactRepository;
+    ModelMapper modelMapper = new ModelMapper();
 
     public void createContact(Contact contact) {
         contactRepository.save(contact);
     }
 
-    public List<Contact> getContacts() {
-        return contactRepository.findAll();
+    public List<ContactDTO> getContacts() {
+        List<Contact> contacts = contactRepository.findAll();
+        List<ContactDTO> contactsDTO = new ArrayList<>();
+        for (Contact c:
+             contacts) {
+            contactsDTO.add(mapToDTO(c));
+        }
+        return contactsDTO;
+    }
+
+    public ContactDTO mapToDTO(Contact contact){
+        return modelMapper.map(contact, ContactDTO.class);
     }
 }
