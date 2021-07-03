eba42dd2734d7ff92e0f6657963c251ca831f226
leonelmperalta <leonelm.peralta@outlook.es>
2021-07-03 15:01:39 -0300

    Envio de mail luego de contacto ok

--- src/main/java/com/restteam/ong/controllers/ContactController.java
+++ src/main/java/com/restteam/ong/controllers/ContactController.java
@@ -1,8 +1,11 @@
 package com.restteam.ong.controllers;
 
 import com.restteam.ong.controllers.dto.ContactDTO;
+import com.restteam.ong.controllers.dto.EmailRequest;
 import com.restteam.ong.models.Contact;
 import com.restteam.ong.services.ContactService;
+import com.restteam.ong.services.EmailService;
+import com.sendgrid.Response;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -18,6 +21,9 @@ public class ContactController {
     @Autowired
     ContactService contactService;
 
+    @Autowired
+    EmailService emailService;
+
     @PostMapping
     public ResponseEntity<String> createContact(@RequestBody Contact contact){
         if(contact.getName() == null || contact.getEmail() == null){
@@ -25,6 +31,15 @@ public class ContactController {
         }
         try {
             contactService.createContact(contact);
+            EmailRequest emailRequest = new EmailRequest();
+            emailRequest.setTo(contact.getEmail());
+            emailRequest.setSubject("Contacto completado con exito.");
+            emailRequest.setBody(String.format("Hola %s! Te informamos que el formulario de contacto se completo con " +
+                    "exito. Desde fundacion somos mas te agradecemos por contactarte. Saludos!", contact.getName()));
+            Response emailResponse = emailService.sendTextEmail(emailRequest);
+            if(emailResponse.getStatusCode() != 200){
+                return new ResponseEntity<>("Contact created succesfully, but informative mail failed to send.", HttpStatus.MULTI_STATUS);
+            }
         } catch (Exception e) {
             return new ResponseEntity<>("We are sorry. Something failed, try again later.", HttpStatus.INTERNAL_SERVER_ERROR);
         }
