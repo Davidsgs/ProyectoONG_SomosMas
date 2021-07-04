d2c7910a69473706175341215210aec147668336
GiseelaDaiana <Gisela.daiana@live.com>
2021-07-04 13:25:41 -0300

    anotation swagger

--- src/main/java/com/restteam/ong/controllers/OrganizationController.java
+++ src/main/java/com/restteam/ong/controllers/OrganizationController.java
@@ -11,7 +11,7 @@ import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
 
-@Controller
+@RestController
 @RequestMapping(path = "/organization/public")
 public class OrganizationController {
     @Autowired
