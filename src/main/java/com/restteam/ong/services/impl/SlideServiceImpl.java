278a92145367c73c5f8742c335741eab58722c11
frueta <askjfuhje@hotmail.com>
2021-07-12 12:46:19 -0300

    -CAMBIOS -agregada la slideServiceImpl.

---
+++ src/main/java/com/restteam/ong/services/impl/SlideServiceImpl.java
@@ -0,0 +1,13 @@
+package com.restteam.ong.services.impl;
+
+import com.restteam.ong.models.Slide;
+import com.restteam.ong.services.SlideService;
+
+public class SlideServiceImpl implements SlideService {
+
+
+    @Override
+    public void addSlide(Slide slide) {
+        //TODO: tengo que pushear ahi lo termino.
+    }
+}
