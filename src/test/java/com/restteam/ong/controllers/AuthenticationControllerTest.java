a3ee5a35556e264ebd2bcec70d21018cd4acb0ab
leonelmperalta <leonelmperalta@outlook.es>
2021-07-13 12:21:31 -0300

    todos los tests de authentication controller estan ok

--- src/test/java/com/restteam/ong/controllers/AuthenticationControllerTest.java
+++ src/test/java/com/restteam/ong/controllers/AuthenticationControllerTest.java
@@ -3,6 +3,7 @@ package com.restteam.ong.controllers;
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.restteam.ong.controllers.dto.AuthenticationRequest;
+import com.restteam.ong.controllers.dto.AuthenticationResponse;
 import com.restteam.ong.controllers.dto.UserRegisterRequest;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.Assertions;
@@ -13,6 +14,7 @@ import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMock
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.MvcResult;
 
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
@@ -32,12 +34,23 @@ class AuthenticationControllerTest {
         return objectMapper.writeValueAsString(object);
     }
 
-    @BeforeEach
-    void setUp() {
-    }
+    private String jwt;
 
-    @AfterEach
-    void tearDown() {
+    @BeforeEach
+    void setUp() throws Exception {
+        //Genero un token valido para poder usarlo despues
+        AuthenticationRequest authRequest = new AuthenticationRequest();
+        authRequest.setPassword("qwerty");
+        authRequest.setUsername("userDeveloper@email.com");
+
+        MvcResult result = mockMvc.perform(post("/auth/login")
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(mapToJSON(authRequest))
+                .characterEncoding("utf-8"))
+                .andReturn();
+
+        AuthenticationResponse authenticationResponse = objectMapper.readValue(result.getResponse().getContentAsByteArray(), AuthenticationResponse.class);
+        jwt = authenticationResponse.getJwt();
     }
 
     @Test
@@ -66,6 +79,8 @@ class AuthenticationControllerTest {
     void createAthenticationTokenWithNotRegisteredEmailReturns404() throws Exception {
         String url = "http://localhost:9800/auth/login";
         AuthenticationRequest request = new AuthenticationRequest();
+        //Aca seteo el email que no existe
+        //El unico mail existente en la bd es: "userDeveloper@email.com"
         request.setUsername("userInvalid@email.com");
         request.setPassword("qwerty");
 
@@ -89,6 +104,7 @@ class AuthenticationControllerTest {
         String url = "http://localhost:9800/auth/login";
         AuthenticationRequest request = new AuthenticationRequest();
         request.setUsername("userDeveloper@email.com");
+        //Aca seteo password no valida, la correcta es : "qwerty"
         request.setPassword("notValid");
 
         Assertions.assertDoesNotThrow(
@@ -154,9 +170,25 @@ class AuthenticationControllerTest {
                 .andExpect(status().isBadRequest())
                 .andReturn();
     }
-/*
+
     @Test
-    void getUserInfo() {
+    void getUserInfoOkReturnsStatus200() throws Exception {
+        String url = "http://localhost:9800/auth/me";
+        mockMvc.perform(get(url)
+                .header("Authorization", String.format("Bearer %s", jwt))
+                .contentType(MediaType.APPLICATION_JSON))
+                .andDo(print())
+                .andExpect(status().isOk())
+                .andReturn();
     }
-     */
+
+    @Test
+    void getUserInfoWithoutAuthorizationReturnsStatus403() throws Exception {
+        String url = "http://localhost:9800/auth/me";
+        mockMvc.perform(get(url)
+                .contentType(MediaType.APPLICATION_JSON))
+                .andDo(print())
+                .andExpect(status().isForbidden())
+                .andReturn();
+    }
 }
