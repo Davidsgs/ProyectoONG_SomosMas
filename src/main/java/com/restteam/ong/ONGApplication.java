26f8e91945883d3ad1a2d18d10f71cfe6ad98d26
David Garcia <davidsamuelgarciasanchez@gmail.com>
2021-07-28 12:50:47 -0300

    Se agregó una Firma al iniciar el programa.

--- src/main/java/com/restteam/ong/ONGApplication.java
+++ src/main/java/com/restteam/ong/ONGApplication.java
@@ -7,7 +7,23 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class ONGApplication{
 
     public static void main(String[] args) {
+
+        String firma = "" +
+                " ------------------------------ Created By: ----------------------------\n" +
+                "8888888b.                   888    888                                   \n" +
+                "888   Y88b                  888    888                                   \n" +
+                "888    888                  888    888                                   \n" +
+                "888   d88P .d88b.  .d8888b  888888 888888 .d88b.   8888b.  88888b.d88b.  \n" +
+                "8888888P\" d8P  Y8b 88K      888    888   d8P  Y8b     \"88b 888 \"888 \"88b \n" +
+                "888 T88b  88888888 \"Y8888b. 888    888   88888888 .d888888 888  888  888 \n" +
+                "888  T88b Y8b.          X88 Y88b.  Y88b. Y8b.     888  888 888  888  888 \n" +
+                "888   T88b \"Y8888   88888P'  \"Y888  \"Y888 \"Y8888  \"Y888888 888  888  888 \n" +
+                "---------------------------------~<o>~----------------------------------";
+
         SpringApplication.run(ONGApplication.class, args);
+        System.out.println(firma);
     }
+
+
 
 }
